b172f68fcdde12b8441b04348d36044b1f09a650
wlthomson <will.tho@outlook.com>
2018-11-17 19:23:26 +1300

    Prettify html, js

--- src/ClientApp.js
+++ src/ClientApp.js
@@ -2,24 +2,24 @@
 var el = React.createElement;
 
 var MyTitle = function(props) {
-    return (
-        el('div', null,
-            el('h1', {style: {color: props.color}}, props.title)
-        )
-    );
+  return el(
+    "div",
+    null,
+    el("h1", { style: { color: props.color } }, props.title)
+  );
 };
 
 var MyFirstComponent = function() {
-    return (
-        el('div', null,
-        el(MyTitle, {color: 'rebeccapurple', title: 'House of Cards'}),
-        el(MyTitle, {color: 'peru', title: 'Orange is the New Black'}),
-        el(MyTitle, {color: 'burlywood', title: 'Stranger Things'})
-        )
-    );
+  return el(
+    "div",
+    null,
+    el(MyTitle, { color: "rebeccapurple", title: "House of Cards" }),
+    el(MyTitle, { color: "peru", title: "Orange is the New Black" }),
+    el(MyTitle, { color: "burlywood", title: "Stranger Things" })
+  );
 };
 
 ReactDOM.render(
-    React.createElement(MyFirstComponent),
-    document.getElementById("app")
+  React.createElement(MyFirstComponent),
+  document.getElementById("app")
 );
