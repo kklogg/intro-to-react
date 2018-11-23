85aa0f79a3a2636afbe708e39eec3199c65cc8dd
wlthomson <will.tho@outlook.com>
2018-11-23 16:10:34 +1300

    Add minimal static http server

---
+++ server.js
@@ -0,0 +1,8 @@
+const connect = require('connect');
+const serveStatic = require('serve-static');
+
+const port = '8080';
+
+connect()
+  .use(serveStatic(__dirname))
+  .listen(port, () => console.log('Server running on 8080...'));
